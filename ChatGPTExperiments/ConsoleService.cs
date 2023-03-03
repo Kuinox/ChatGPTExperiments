155e0ae7fa8e655f709b07463e442dc47463d894
Kuinox <github@kuinox.io>
2023-03-03 01:53:16 +0100

    initial commit

---
+++ ChatGPTExperiments/ConsoleService.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Options;
+using OpenAI_API;
+using OpenAI_API.ChatCompletions;
+using OpenAI_API.Models;
+
+namespace ChatGPTExperiments
+{
+    public class ConsoleService : BackgroundService
+    {
+        readonly IOptions<OpenAPIConfig> _config;
+        OpenAIAPI? _api;
+
+        public ConsoleService(IOptions<OpenAPIConfig> config)
+        {
+            _config = config;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _api = new OpenAIAPI(_config.Value.ApiKey);
+            return base.StartAsync(cancellationToken);
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    var line = await Console.In.ReadLineAsync().WaitAsync(stoppingToken);
+                    var result = await _api!.ChatCompletion.CreateCompletionAsync(new ChatCompletionRequest()
+                    {
+                        Model = Model.GPT35Turbo,
+                        Messages = new ChatMessage[]
+                        {
+                            new ChatMessage
+                            {
+                                Role = "system",
+                                Content = "You are casualy conversating with humans."
+                            },
+                            new ChatMessage
+                            {
+                                Role = "user",
+                                Content = line
+                            }
+                        }
+                    });
+                    Console.WriteLine(result.Choices.Single().Message.Content);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+    }
+}
---
+++ ChatGPTExperiments/Program.cs
@@ -0,0 +1,30 @@
+﻿using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using OpenAI_API;
+namespace ChatGPTExperiments
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("appsettings.json", optional: false);
+                })
+                .ConfigureLogging(x =>
+                {
+                    x.AddSimpleConsole();
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.Configure<DiscordSocketConfig>(hostContext.Configuration.GetSection("DiscordSocketConfig"))
+                        .Configure<OpenAPIConfig>(hostContext.Configuration.GetSection("OpenAPIConfig"))
+                        .AddSingleton<DiscordSocketClient>();
+                })
+                .Build();
+
+            await host.RunAsync();
+        }
+    }
+}
