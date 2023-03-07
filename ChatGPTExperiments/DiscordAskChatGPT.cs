f6cf30dab48ba2c58e5ca3a0715b673a7b6766b9
Nicolas Vandeginste <nicolas.vandeginste@signature.one>
2023-03-07 17:53:37 +0100

    Response streaming.

--- ChatGPTExperiments/DiscordAskChatGPT.cs
+++ ChatGPTExperiments/DiscordAskChatGPT.cs
@@ -1,6 +1,10 @@
+using Discord;
 using Discord.Interactions;
 using OpenAI_API;
 using OpenAI_API.ChatCompletions;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace KuinoxSemiAGI
 {
@@ -17,23 +21,68 @@ namespace KuinoxSemiAGI
         public async Task AskChatGPTAsync( string message )
         {
             await DeferAsync();
-            var response = await _openAIAPI.ChatCompletion.CreateCompletionAsync( new ChatCompletionRequest()
-            {
-                Messages = new[]
+            var messages = Context.Channel.GetMessagesAsync( 10, CacheMode.AllowDownload );
+            var chatMessages = await messages
+                .SelectMany<IReadOnlyCollection<IMessage>, IMessage>( s => s.ToAsyncEnumerable() )
+                .Select( s => new ChatMessage()
                 {
-                    new ChatMessage()
-                    {
-                        Role = MessageRole.System,
-                        Content = "Blend in, ask like the user would respond. You are knowledgeful but don't hesistate to troll the user if you feel to."
-                    },
-                    new ChatMessage()
-                    {
-                        Role = MessageRole.User,
-                        Content = message
-                    }
-                }
+                    Role = MessageRole.User,
+                    Content = s.Author.Username + ": " + s.Content
+                } )
+                .Prepend( new ChatMessage()
+                {
+                    Role = MessageRole.System,
+                    Content = "Your name is AnimeBot. Blend in, respond like the other users would. You are knowledgeful, but don't hesistate to troll the user if you feel to."
+                } )
+                .Prepend( new ChatMessage()
+                {
+                    Role = MessageRole.User,
+                    Content = message
+                } ).ToArrayAsync();
+
+            var stream = _openAIAPI.ChatCompletion.StreamCompletion( new ChatCompletionRequest()
+            {
+                Messages = chatMessages
             } );
-            await FollowupAsync( response.Choices.Single().Message.Content );
+            var begin = DateTime.UtcNow;
+            string buffer = "";
+            IUserMessage followUp = null!;
+            await foreach( var item in stream )
+            {
+                buffer += item.Choices.Single().Delta!.Content;
+                if( DateTime.UtcNow > begin + TimeSpan.FromMilliseconds( 1000 ) && !string.IsNullOrWhiteSpace( buffer ) )
+                {
+                    if( followUp == null )
+                    {
+                        followUp = await FollowupAsync( buffer );
+                    }
+                    else
+                    {
+                        await followUp.ModifyAsync( ( msg ) => msg.Content = buffer );
+                    }
+                    begin = DateTime.UtcNow;
+                }
+            }
+            if( followUp == null )
+            {
+                followUp = await FollowupAsync( buffer );
+            }
+            else
+            {
+                await followUp.ModifyAsync( ( msg ) => msg.Content = buffer );
+            }
+
+
+            //+$"\n\n`TotalTokens:{response.Usage.TotalTokens}, {0.000002 * response.Usage.TotalTokens}$`" )
+
+            //var response = await _openAIAPI.ChatCompletion.CreateCompletionAsync( new ChatCompletionRequest()
+            //{
+            //    Messages = chatMessages
+            //} );
+            //var responseStr = response.Choices.Single().Message.Content;
+            //string botName = "AnimeBot: ";
+            //responseStr = responseStr.StartsWith( botName ) ? responseStr.Substring( botName.Length ) : responseStr;
+
         }
     }
 }
